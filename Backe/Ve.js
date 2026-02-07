f48e161c433b62fa80717fc7d5413f62563feac6
jineshchougule5712-droid <jineshchougule5712@gmail.com>
2026-02-07 09:42:07 +0530

    Update Ve.js

--- Backe/Ve.js
+++ Backe/Ve.js
@@ -59,7 +59,10 @@ app.post(
 
     }
 );
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
+
